2bead756f0ae4c6ba27f5dc5ff162ae406f672d0
leonardo <leonardodias@ucl.br>
2021-12-04 08:10:00 -0300

    Initial setup

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import axios from 'axios'
+
+const Greeting = () => {
+  return (
+    <div>
+      <p>This is a random greeting:</p>
+    </div>
+  )
+}
+
+export default Greeting
